102c57b977e58fe7a369ddcfab20c7052a0428a3
Billiam Pepsiford <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2018-04-18 02:27:12 +0000

    I'M SO AMAZING; test environment complete, program able to differentiate between dits, dahs, and the start of new words.

--- Telegraph.py
+++ Telegraph.py
@@ -1,29 +1,30 @@
 import RPi.GPIO as GPIO
+import time
 
 #Class that handles the keying of characters
+
+#Based off of this graphic: https://en.wikipedia.org/wiki/Morse_code#/media/File:International_Morse_Code.svg
+#Length of a "dit"
+UNIT_LENGTH = 0.07 #The length of one morse code "unit"
+#Length of a "dah," typically 3 times greater than the length of a "dit"
+DASH_LENGTH = UNIT_LENGTH * 3 #0.21
+#Length of time between parts of the same letter
+LETTER_PART_SPACE_LENGTH = UNIT_LENGTH #0.07
+#Length of time between different letters
+LETTER_SPACE_LENGTH = UNIT_LENGTH * 3
+#Length of time between words
+WORD_SPACE_LENGTH = UNIT_LENGTH * 7
+DOT = '.'
+DASH = '-'
+
 class TelegraphKey(object):
-	
-	#Based off of this graphic: https://en.wikipedia.org/wiki/Morse_code#/media/File:International_Morse_Code.svg
-	#Length of a "dit"
-	UNIT_LENGTH = 0.07 #The length of one morse code "unit"
-	#Length of a "dah," typically 3 times greater than the length of a "dit"
-	DASH_LENGTH = UNIT_LENGTH * 3 #0.21
-	#Length of time between parts of the same letter
-	LETTER_PART_SPACE_LENGTH = UNIT_LENGTH #0.07
-	#Length of time between different letters
-	LETTER_SPACE_LENGTH = UNIT_LENGTH * 3
-	#Length of time between words
-	WORD_SPACE_LENGTH = UNIT_LENGTH * 7
-	DOT = '.'
-	DASH = '-'
-	
 
 	#pin -> the pin the button runs to to provide output.
 	def __init__(self, pin):
 		self.pin = pin
 
 	def key_character(self):
-		unit = key_unit()
+		unit = self.key_unit()
 		
 		if unit > UNIT_LENGTH and unit < DASH_LENGTH:
 			print("DIT")
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import RPi.GPIO as GPIO
-import time
+from Telegraph import TelegraphKey
 
 PIN = 17
-DIT_LENGTH = 0.07
-DAH_LENGTH = DIT_LENGTH * 3 #0.21
-
-#Returns the length of time for which the button was pressed
-def listen():
-    
-    while not GPIO.input(PIN):
-        pass
-    
-    start = time.time()
-    
-    while GPIO.input(PIN):
-        pass
-    
-    return time.time() - start        
 
 if __name__ == "__main__":
     #Enter broadcom pin numbering mode
@@ -24,8 +9,10 @@ if __name__ == "__main__":
     
     #Setting the default to PUD_DOWN makes the button default to an off
     #position, programatically.
-    GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    
+    key = TelegraphKey(PIN)
     
     while True:
-        print(listen())
+        key.key_character()
 	
