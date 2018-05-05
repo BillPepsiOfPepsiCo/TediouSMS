59e14139271464dfe07ab1ebd1707b612e7b5060
BillPepsi <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2018-05-05 14:06:07 -0500

    uhhh fixed script for the last time bet

--- tediousms/install_dependencies.sh
+++ tediousms/install_dependencies.sh
@@ -3,7 +3,7 @@
 echo "Installing dependencies"
 apt-get update
 apt-get -y install python3-rpi.gpio
-apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 
 echo "Installing Python 3.6.5"
 wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
@@ -16,15 +16,18 @@ make altinstall
 echo "Cleaning up"
 rm -r Python-3.6.5
 rm Python-3.6.5.tar.xz
-apt-get --purge remove build-essential tk-dev
-apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-apt-get autoremove
-apt-get clean
+apt-get -y --purge remove build-essential tk-dev
+apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+apt-get -y autoremove
+apt-get -y clean
 
-echo "Installing pip"
-apt-get install python3-pip
+echo "Upgrading pip3.6"
+pip3.6 install --upgrade pip
+
+echo "Installing RPi.GPIO"
+pip3.6 install RPi.GPIO
 
 echo "Installing websockets"
-pip3 install websockets
+pip3.6 install websockets
